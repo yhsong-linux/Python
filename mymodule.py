fc6eb8d49c0e84b51ae20656dca1480c329fc3f0
Song, Yonghong <yhsong@linx-info.com>
2015-09-06 16:12:55 +0800

    Create my own module

---
+++ mymodule.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3.1
+# Filename : mymodule.py
+
+def sayhi():
+	print('Hi, this is mymodule speaking.')
+
+version = '0.1'
+
+if __name__ == '__main__':
+	print('This program is being run by itself')
+else:
+	print('I am being imported from another module')
+
+# End of mymodule.py
