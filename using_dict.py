4c3cc1ab7d3138713863d63990d24dddc5ebf1ed
Song, Yonghong <yhsong@linx-info.com>
2015-09-07 10:03:07 +0800

    Equivalent Conditions test determines whether the dictionary contains the keys

--- using_dict.py
+++ using_dict.py
@@ -19,5 +19,7 @@ print('\nThere are %d contacts in the address-book\n' % len(ab))
 for name,address in ab.items():
 	print('Contact %s at %s' % (name, address))
 
-if 'Guido' in ab:
+# "if .. in .." Equivalent to "D.__contains__('key')"
+#if 'Guido' in ab: 
+if ab.__contains__('Guido'):
 	print('\nGuido\'s address is %s' % ab['Guido'])
