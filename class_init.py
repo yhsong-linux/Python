bd36c60cefdfba5983113b17277d5300df4671e3
Song, Yonghong <yhsong@linx-info.com>
2015-09-07 11:47:37 +0800

    Use __init__ method

---
+++ class_init.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3.1
+# Filename: class_init.py
+
+class Person:
+	def __init__(self, name):
+		self.name = name
+	
+	def sayHi(self):
+		print('Hello, my name is', self.name)
+
+p = Person('Swaroop')
+p.sayHi()
