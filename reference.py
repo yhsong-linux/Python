ba90afe75712a26bbb6f33d18a1c5f3545c21e32
Song, Yonghong <yhsong@linx-info.com>
2015-09-07 10:30:17 +0800

    Object and reference

---
+++ reference.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3.1
+# Filename : reference.py
+
+print('Simple Assignment')
+shoplist = ['apple', 'mango', 'carrot', 'banana']
+mylist = shoplist
+
+del shoplist[0]
+print('shoplist is', shoplist)
+print('mylist is', mylist)
+
+print('Copy by making a full slice')
+mylist = shoplist[:]
+del mylist[0]
+print('shoplist is', shoplist)
+print('mylist is', mylist)
